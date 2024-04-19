08f4cad7e918b7aa06530fdc449029fc417856e0
Ayushi <s109098@ltisdschools.net>
2024-04-19 16:00:07 -0500

    changes to script for the health bar

--- Assets/SceneLinkWeapon.cs
+++ Assets/SceneLinkWeapon.cs
@@ -10,17 +10,34 @@ public class changeScene : MonoBehaviour
     public int manaMCamt = 100;
     public int manaOppamt = 100;
     public TMP_Text manaMC;
+
     //public OnButtonClick onButtonClick;
-    
+    void Start()
+    {
+        if(manaMC != null)
+        {
+            manaMC.text = manaMCamt.ToString();
+        }
+    }
+    /*void Update()
+    {
+        manaMC.text = manaMC.ToString();
+    }*/
     public void MoveToScene(int sceneID)
     {
         SceneManager.LoadScene(sceneID);
-        manaMC.text = manaMCamt.ToString();
     }
     public void healAMT(int amt)
     {
-        manaMCamt -= amt;
-        //manaMC.text = manaMCamt.ToString();
-        
+        manaMCamt += amt;
+        manaMC.text = manaMCamt.ToString();
+        Debug.Log(manaMCamt);
+        manaMC.text = manaMCamt.ToString();
+
+    }
+    public void substractScore(int subAmt)
+    {
+        manaMCamt -= subAmt;
+        manaMC.text = manaMCamt.ToString();
     }
 }
