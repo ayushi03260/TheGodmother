67e6b67b4b52b3e0e035f02e57c9ad80d102b420
Ayushi <s109098@ltisdschools.net>
2024-05-10 10:28:58 -0500

    changed the health bar

--- Assets/Scripts/updateMcText.cs
+++ Assets/Scripts/updateMcText.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEditor;
+//using System;
 
 public class updateMcText : MonoBehaviour
 {
     public TMP_Text mcText;
-    //public TMP_Text oppText;
+    public TMP_Text oppText;
+    public TMP_Text useText;
     public static int amount;
+    int randAmt;
     //public static int oppAmount;
     //public GameManager script = new GameManager();
     // Start is called before the first frame update
@@ -22,10 +25,31 @@ public class updateMcText : MonoBehaviour
     void Update()
     {
         //int mcAmt = script.manaMCamt;
-        mcText.text = amount.ToString();
+        if (amount > 0)
+        {
+            mcText.text = amount.ToString();
+            randAmt = oppchosen();
+
+            Invoke("function", 5f);
+
+        }
+        
         //oppText.text = oppAmount.ToString();
         //Debug.Log(amount.ToString());
     }
+    public int oppchosen()
+    {
+        int[] oppAmount = new int[] {-5, 0};
+        return oppAmount[Random.Range(0,1)];
+    }
 
+    void function()
+    {
+        useText.text = "Opp used " + randAmt.ToString();
+        
+        Debug.Log(amount + randAmt);
+        randAmt = 0;
+        //mcText.text = amount.ToString();
+    }
 
 }
